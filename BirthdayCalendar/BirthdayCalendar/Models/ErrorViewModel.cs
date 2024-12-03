0b291a4dad7bc61eb2e6ead7d5b523f3a303ac5b
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2024-12-03 19:44:21 +0200

    Added MockBirthdaysRepository and deleted Error view

--- BirthdayCalendar/BirthdayCalendar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BirthdayCalendar.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
