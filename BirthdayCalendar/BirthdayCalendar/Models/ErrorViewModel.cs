a86cf9caca36a320a8053afbaed7ca1f21283e64
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2024-12-03 17:30:09 +0200

    The start of the birthday project( configuration files)

---
+++ BirthdayCalendar/BirthdayCalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirthdayCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
