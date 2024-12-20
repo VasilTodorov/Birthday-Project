126c612b0756f05d1756f698037cd1db15fe0dc3
Васил Тодоров <vasilit@office365student.uni-sofia.bg>
2024-12-20 10:21:32 +0200

    Added POCO class Event

---
+++ BirthdayCalendar/BirthdayCalendar/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace BirthdayCalendar.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; } // Primary Key
+        public string GoogleEventId { get; set; } = string.Empty; // Google Calendar Event ID
+        public string Summary { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Location { get; set; }
+        public string? TimeZone { get; set; }        
+    }
+}
--- BirthdayCalendar/BirthdayCalendar/Models/User.cs
+++ BirthdayCalendar/BirthdayCalendar/Models/User.cs
@@ -5,7 +5,8 @@
         public int UserId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string? LastName { get; set; }
-        public string? Email { get; set; }
+        public string? EmailAddress { get; set; }
         public string? PhoneNumber { get; set; }
+        
     }
 }
